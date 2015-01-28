69c6dfb5202477bafa715c7907e7b2bac74716d4
RunliSong <396130319@qq.com>
2015-01-28 19:10:25 +1100

    RS update file

---
+++ Calendar/Calendar/MonthInYearCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MonthInYearCollectionViewCell.h
+//  Calendar
+//
+//  Created by RunliSong on 15/1/28.
+//  Copyright (c) 2015年 Deakin University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MonthInYearCollectionViewCell : UICollectionViewCell
+@property (strong, nonatomic) IBOutlet UILabel *myWeekDays;
+@property (strong, nonatomic) IBOutlet UITextView *myDaysInMouth;
+
+@end
