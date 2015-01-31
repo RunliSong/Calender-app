ef5991bf9a63ae35daac9728427e1b16fec1cf1a
Alex Gao <alexgao11@gmail.com>
2015-01-31 14:16:17 +1100

    Delete A tableviewcontroller, will add back soon

--- Calendar/Calendar/EventSearchViewController.m
+++ Calendar/Calendar/EventSearchViewController.m
@@ -8,7 +8,7 @@
 
 #import "EventSearchViewController.h"
 #import "Utilities.h"
-#import "ResultsTableViewController.h"
+
 
 
 @interface EventSearchViewController ()
@@ -26,23 +26,25 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-- (IBAction)searchResult:(UIButton *)sender {
-    NSArray *results = [Utilities getEventsWithTitle:_eventTitle.text description:_keyword.text startDate:_fromDate.date andEndDate:_toDate.date];
-    if (results.count) {
-        //go to show result
-        ResultsTableViewController *resultController = [[ResultsTableViewController alloc] init];
-        //resultController.events = results;
-        [self presentViewController:resultController animated:YES completion:nil];
-    }
-    else {
-        NSString *alertTitle = @"Can't find any event meets the condition";
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Can't find anything" message:alertTitle delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
-        [alert show];
-    }
-}
+
 - (IBAction)backToDayView:(UIButton *)sender {
     // back to day view
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)searchResult:(id)sender {
+        NSArray *results = [Utilities getEventsWithTitle:_eventTitle.text description:_keyword.text startDate:_fromDate.date andEndDate:_toDate.date];
+        if (results.count) {
+            //go to show result
+//            ResultsTableViewController *resultController = [[ResultsTableViewController alloc] init];
+//            //resultController.events = results;
+//            [self presentViewController:resultController animated:YES completion:nil];
+//    
+        }
+        else {
+            NSString *alertTitle = @"Can't find any event meets the condition";
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Can't find anything" message:alertTitle delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+            [alert show];
+        }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -50,14 +52,27 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+#pragma mark - Navigation
+//
+//// In a storyboard-based application, you will often want to do a little preparation before navigation
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+//    // Get the new view controller using [segue destinationViewController].
+//    // Pass the selected object to the new view controller.
+//    NSArray *results = [Utilities getEventsWithTitle:_eventTitle.text description:_keyword.text startDate:_fromDate.date andEndDate:_toDate.date];
+//    if (results.count) {
+//        //go to show result
+//        ResultsTableViewController *resultController = [[ResultsTableViewController alloc] init];
+//        //resultController.events = results;
+//        [self presentViewController:resultController animated:YES completion:nil];
+//        
+//    }
+//    else {
+//        NSString *alertTitle = @"Can't find any event meets the condition";
+//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Can't find anything" message:alertTitle delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+//        [alert show];
+//    }
+//}
+
 
 @end
