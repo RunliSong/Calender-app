49146db1ee2efc4d2c8a35289f89797a66e793e0
Alex Gao <alexgao11@gmail.com>
2015-02-05 17:14:53 +1100

    Update search view controller's auto layout

--- Calendar/Calendar/SearchEventViewController.m
+++ Calendar/Calendar/SearchEventViewController.m
@@ -11,13 +11,14 @@
 #import "Utilities.h"
 #import "EditViewController.h"
 
-@interface SearchEventViewController ()
+@interface SearchEventViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *eventTitle;
 @property (weak, nonatomic) IBOutlet UITextField *eventKeyword;
 @property (weak, nonatomic) IBOutlet UIDatePicker *fromDate;
 @property (weak, nonatomic) IBOutlet UIDatePicker *toDate;
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 - (IBAction)backToPrevious:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *fromDateText;
+@property (weak, nonatomic) IBOutlet UITextField *endDateText;
 
 @end
 
@@ -25,33 +26,43 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    
+    // initialize all UIs
+    // set font color
     [_fromDate setValue:[UIColor whiteColor] forKeyPath:@"textColor"];
     [_toDate setValue:[UIColor whiteColor] forKeyPath:@"textColor"];
+    NSDateFormatter *fmt = [NSDateFormatter new];
+    [fmt setDateFormat:@"dd-MMMM-yyyy"];
+    
+    // default date is today
+    [_fromDateText setText:[fmt stringFromDate:[NSDate date]]];
+    [_endDateText setText:[fmt stringFromDate:[NSDate date]]];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // add gesture for recognizer for the text field
+    // add gesture for recognizer for the text field for dismiss keyboard
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
                                    initWithTarget:self
                                    action:@selector(dismissKeyboard)];
     
     [self.view addGestureRecognizer:tap];
-    // Do any additional setup after loading the view.
+    _fromDateText.delegate = self;
+    _endDateText.delegate = self;
+    
 }
-
-// dismiss keyboard when touches outside
-//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-//    [self.view endEditing:YES];
-//}
 
 -(void)dismissKeyboard {
     [_eventTitle resignFirstResponder];
     [_eventKeyword resignFirstResponder];
+    [_fromDate setHidden:YES];
+    [_toDate setHidden:YES];
 }
 
+// search event, if no result fount, gives user a hint, otherwise navigate to result controller
 - (IBAction)search:(id)sender {
+    
     NSArray *result = [Utilities getEventsWithTitle:_eventTitle.text description:_eventKeyword.text startDate:_fromDate.date andEndDate:_toDate.date];
     if ([result count]) {
         UIStoryboard *krisStory = [UIStoryboard storyboardWithName:@"Kris" bundle:nil];
@@ -65,6 +76,7 @@
     }
 }
 
+//navigate to result controller
 - (IBAction)searchEvent:(UIButton *)sender {
     UIStoryboard *krisStory = [UIStoryboard storyboardWithName:@"Kris" bundle:nil];
     SearchEventViewController *sevc = [krisStory instantiateViewControllerWithIdentifier:@"Search"];
@@ -72,11 +84,40 @@
     [self presentViewController:sevc animated:YES completion:nil];
 }
 
+// add event controller
 - (IBAction)addEvent:(UIButton *)sender {
     UIStoryboard *rexStory = [UIStoryboard storyboardWithName:@"Rex" bundle:nil];
     EditViewController *evc = [rexStory instantiateViewControllerWithIdentifier:@"rex.storyboard"];
     evc.createOrUpdate = Create;
     [self presentViewController:evc animated:YES completion:nil];
+}
+
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    if (textField == self.fromDateText) {
+        self.fromDate.hidden = NO;
+        self.toDate.hidden = YES;
+    }
+    if (textField == self.endDateText) {
+        self.toDate.hidden = NO;
+        self.fromDate.hidden = YES;
+    }
+    return TRUE;
+}
+
+
+// if value changes, update text field's value
+- (IBAction)fromDateValueChanged:(UIDatePicker *)sender {
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    [formatter setDateFormat:@"dd-MMMM-yyyy"];
+    _fromDateText.text = [formatter stringFromDate:sender.date];
+    NSLog(@"Value changed");
+}
+
+- (IBAction)endDateValueChanged:(UIDatePicker *)sender {
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    [formatter setDateFormat:@"dd-MMMM-yyyy"];
+    _endDateText.text = [formatter stringFromDate:sender.date];
+    NSLog(@"Value changed");
 }
 
 
@@ -86,15 +127,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)backToPrevious:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
